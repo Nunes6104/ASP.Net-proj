a54156631eb36e51660f9ad8328b90dc7ec9d6c8
Nunes6104 <jmmnunes6104@gmail.com>
2025-03-29 09:39:19 +0000

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Aula_1()
+        {
+            ViewBag.escola = "Istec";
+            ViewData["turma"] = "Turma A";
+            TempData["disciplina"] = "Tecnologias de Internet III";
+
+            Aluno ze = new Aluno() {Num = 1, Nome = "Zé", Turma = "A" };
+            return View(ze);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+	public class Aluno
+	{
+        public int Num { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+    }
+}
