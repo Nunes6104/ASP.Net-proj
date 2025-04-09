35c23edae3e304989a2a97ad72296e1f4824d839
Nunes6104 <jmmnunes6104@gmail.com>
2025-04-09 10:06:06 +0100

    asp update features

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,12 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult EditAluno(int ? num)
+        {
+            BD bd= new BD();
+            Aluno aluno = bd.GetAlunos().Where(x => x.Num == (num?? -1)).FirstOrDefault();
+            if (aluno != null)
+            {
+                return View(aluno);
+            }
+            else
+            {
+                return RedirectToAction("Alunos", new { message = "Num não existe" });
+            }
+        }
+        [HttpPost]
+        public ActionResult EditAluno(Aluno editado)
+        {
+            BD bd = new BD();
+            Aluno aluno = bd.GetAlunos().Where(x => x.Num == editado.Num).FirstOrDefault();
+            if (aluno != null)
+            {
+                aluno.Nome = editado.Nome;
+                aluno.Turma = editado.Turma;
+                bd.gravar();
+                return RedirectToAction("Alunos", new { message = "Alterado com sucesso" });
+            }
+            else
+            {
+                return RedirectToAction("Alunos", new { message = "Num não existe" });
+            }
+        }
+
+        public ActionResult DeleteAluno(int ? num)
+        {
+            BD bd = new BD();
+            Aluno del = bd.GetAlunos().Where(x => x.Num == (num ?? - 1)).FirstOrDefault();
+            if (del != null)
+            {
+                return View(del);
+            }
+            else
+            {
+                return RedirectToAction("Alunos", new { message = "Num não existe" });
+            }
+        }
+        [HttpPost]
+        [ActionName("DeleteAluno")]
+        public ActionResult Delete(int? num)
+        {
+            BD bd = new BD();
+            Aluno del = bd.GetAlunos().Where(x => x.Num == (num ?? -1)).FirstOrDefault();
+            if (del != null)
+            {
+                bd.GetAlunos().Remove(del);
+                bd.gravar();
+                return RedirectToAction("Alunos", new { message = "Apagado com sucesso" });
+            }
+            else
+            {
+                return RedirectToAction("Alunos", new { message = "Num não existe" });
+            }
+        }
+
+        public ActionResult CreateAluno()
+        {
+            Aluno aluno = new Aluno();
+            return View(aluno);
+        }
+        [HttpPost]
+        public ActionResult CreateAluno(Aluno novo)
+        {
+            BD bd = new BD();
+            bd.GetAlunos().Add(novo);
+            bd.gravar();
+            return RedirectToAction("Alunos", new { message = "Registo Inserido com sucesso" });
+        }
+
+        public ActionResult DetailsAluno(int? num)
+        {
+            BD bd = new BD();
+            Aluno aluno = bd.GetAlunos().Where(x => x.Num == (num ?? -1)).FirstOrDefault();
+            if (aluno != null)
+            {
+                return View(aluno);
+            }
+            else
+            {
+                return RedirectToAction("Alunos", new { message = "Num não existe" });
+            }
+        }
+        public ActionResult Alunos(String message)
+        {
+            ViewBag.message = message;
+            BD bd = new BD();
+            List<Aluno> alunos = bd.GetAlunos();
+            return View(alunos);
+        }
+
+        public ActionResult Dobro(int ? num)
+        {
+            return Json(new { Dobro = 2 * num ?? 0 }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
         public ActionResult Aula_1()
         {
             ViewBag.escola = "Istec";
@@ -16,6 +120,14 @@ namespace WebApplication1.Controllers
             TempData["disciplina"] = "Tecnologias de Internet III";
 
             Aluno ze = new Aluno() {Num = 1, Nome = "Zé", Turma = "A" };
+            return View(ze);
+        }
+
+        [HttpPost]
+        public ActionResult Aula_1(int? txtnum)
+        {
+            Aluno ze = new Aluno() { Num = 1, Nome = "Zé", Turma = "A" };
+            ViewBag.dobro = 2 * txtnum ?? 0;
             return View(ze);
         }
         public ActionResult Index()
--- WebApplication1/Models/Aluno.cs
+++ WebApplication1/Models/Aluno.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace WebApplication1.Models
 {
@@ -10,5 +12,63 @@ namespace WebApplication1.Models
         public int Num { get; set; }
         public string Nome { get; set; }
         public string Turma { get; set; }
+    }
+
+    public class Exam
+    {
+        public int Num { get; set; }
+        public string Disciplina { get; set; }
+        public int Nota { get; set; }
+    }
+
+    public class BD
+    {
+        List<Aluno>alunos;
+        List<Exam>exames;
+        public void gravar()
+        {
+            HttpServerUtility server = HttpContext.Current.Server;
+            String path = server.MapPath(@"~/App_Data/dados.json");
+            DataContractJsonSerializer dc = new DataContractJsonSerializer(typeof(List<Aluno>));
+            FileStream fs = new FileStream(path,FileMode.Create,FileAccess.Write);
+            dc.WriteObject(fs, alunos);
+            fs.Close();
+        }
+
+        public void carregar()
+        {
+            HttpServerUtility server = HttpContext.Current.Server;
+            String path = server.MapPath(@"~/App_Data/dados.json");
+            if (File.Exists(path))
+            {
+                DataContractJsonSerializer dc = new DataContractJsonSerializer(typeof(List<Aluno>));
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                alunos = (List<Aluno>)dc.ReadObject(fs);
+                fs.Close();
+            }
+            else
+            {
+                alunos = new List<Aluno>();
+            }
+
+        }
+
+        public BD()
+        {
+            //alunos = new List<Aluno>
+            //{
+            //    new Aluno {Num =1, Nome="João", Turma="A" },
+            //    new Aluno {Num =2, Nome="Mira", Turma="A" },
+            //    new Aluno {Num =3, Nome="Leandro", Turma="B" },
+            //    new Aluno {Num =4, Nome="Francisco", Turma="A" },
+            //    new Aluno {Num =1, Nome="Diogo", Turma="B" }
+            //};
+            carregar();
+        }
+
+        public List<Aluno> GetAlunos()
+        {
+            return alunos;
+        }
     }
 }
